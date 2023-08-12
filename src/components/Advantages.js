7512ad9b63a2bc862624d24f9f1a817c129a7408
Aditya Rai <ar@pop-os.localdomain>
2023-08-12 15:02:03 +0530

    first commit

---
+++ src/components/Advantages.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Advantage.css'
+
+export default function Advantages() {
+    const features = [
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        },
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        },
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        },
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        },
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        },
+        {
+            "img":"",
+            "feature":"",
+            "desc":""
+        }
+    ]
+  return (
+    <div className='container'>
+      <h4 style={{display:'flex',justifyContent:'center'}}>---Why Us---</h4>
+      <div className='advantageContainer'>
+        <div>
+            <img src="" alt="" />
+            <h6>This is a feature</h6>
+        </div>
+        <div className='descriptionBox'>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Slider.js
@@ -0,0 +1,36 @@
+import React from 'react'
+export default function Slider() {
+    
+  return (
+    <>
+    <div className="container">
+    <div id="carouselExampleIndicators" className="carousel slide">
+        <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner my-4">
+            <div className="carousel-item active">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Fashion/Event/Gateway/JuneWRS/Teaser/Unrec-PC-2-3000._CB587224584_.jpg" className="d-block w-100" alt="..."/>
+            </div>
+            <div className="carousel-item">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Toys/PC_Hero_2x-toys._CB591544420_.jpg" className="d-block w-100" alt="..."/>
+            </div>
+            <div className="carousel-item">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Shreyansh/BAU/Unrexc/D70978891_INWLD_BAU_Unrec_Uber_PC_Hero_3000x1200._CB594707876_.jpg" className="d-block w-100" alt="..."/>
+            </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
+    </>
+  )
+}
